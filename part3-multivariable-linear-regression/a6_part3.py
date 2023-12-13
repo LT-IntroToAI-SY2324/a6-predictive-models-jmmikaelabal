f64539a56e074eede2d2ba605d30620f7e224d45
Jehan Balading <jmmikaelabal@cps.edu>
2023-12-13 10:41:03 -0600

    part2 and part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -2,21 +2,46 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
-#imports and formats the data
+# imports and formats the data
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x = data[["miles","age"]].values
+x = data[["miles", "age"]].values
 y = data["Price"].values
 
-#split the data into training and testing data
+# split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-#create linear regression model
+# create linear regression model
+model = LinearRegression()
 
-#Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
+# fit the model with training data
+model.fit(x_train, y_train)
 
+# Find and print the coefficients, intercept, and r squared values.
+# Each should be rounded to two decimal places.
+coefficients = np.round(model.coef_, 2)
+intercept = np.round(model.intercept_, 2)
+r_squared = np.round(model.score(x_test, y_test), 2)
 
-#Loop through the data and print out the predicted prices and the 
-#actual prices
+print(f"Coefficients: {coefficients}")
+print(f"Intercept: {intercept}")
+print(f"R-squared: {r_squared}")
+
+# Loop through the data and print out the predicted prices and the
+# actual prices
 print("***************")
 print("Testing Results")
+predictions = model.predict(x_test)
+for i in range(len(predictions)):
+    print(f"Predicted Price: {np.round(predictions[i], 2)}, Actual Price: {y_test[i]}")
+
+# Predict the price for a 10-year-old car with 89,000 miles
+car1_features = np.array([[89000, 10]])  # miles, age
+predicted_price_car1 = model.predict(car1_features)
+print(f"Predicted Price for a 10-year-old car with 89,000 miles: ${np.round(predicted_price_car1[0], 2)}")
+
+# Predict the price for a 20-year-old car with 150,000 miles
+car2_features = np.array([[150000, 20]])  # miles, age
+predicted_price_car2 = model.predict(car2_features)
+print(f"Predicted Price for a 20-year-old car with 150,000 miles: ${np.round(predicted_price_car2[0], 2)}")
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,10 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+    My R squared coefficient is .85, this means that my data can be represented well throughout my model.
 2. Is your model accurate? Why or why not?
-
+My model is accurate because the R square value is close to 1.
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
+8983.56 dollars; 1912.18 dollars
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening? 
+The older the car, the more miles it would have. This also means it decreases in value. I think that is why it leads to negative values for some of my predicted results.
